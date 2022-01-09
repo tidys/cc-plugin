69c1eb99c9a369d510b9f874916fc18f7de8ed9b
xyf-mac <xu_yanfeng@126.com>
2022-01-09 17:08:12 +0800

    完善v3的require方法

--- src/ccp/entry-render.ts
+++ src/ccp/entry-render.ts
@@ -1,8 +1,9 @@
-import { CocosPluginConfig, CocosPluginManifest, CocosPluginOptions, PluginMainWrapper } from '../declare';
+import { CocosPluginConfig, CocosPluginManifest, CocosPluginOptions, PluginVersion } from '../declare';
 import ClientSocket from './client-socket';
 import { Panel } from './panel'
 
 import { Port } from './index'
+import * as Path from 'path';
 
 interface PanelOptions {
     ready: (rootElement: any, args: any) => void;
@@ -14,8 +15,16 @@ export class CocosCreatorPluginRender {
     public Panel: Panel = new Panel();
 
     require(name: string): any {
-        // @ts-ignore
-        return Editor.require(`packages://${this.manifest!.name}/node_modules/${name}`)
+        const { version } = this.options!;
+
+        if (version === PluginVersion.v2) {
+            // @ts-ignore
+            return Editor.require(`packages://${this.manifest!.name}/node_modules/${name}`)
+        } else if (version === PluginVersion.v3) {
+            // @ts-ignore
+            let editorModules = Path.join(Editor.App.path, 'node_modules');
+            module.paths.push(editorModules);
+        }
     }
 
     public init(config: CocosPluginConfig, options: PanelOptions) {
