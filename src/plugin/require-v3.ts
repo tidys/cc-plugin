4f607dc63e791dabde349cf6c3bb1ed43f82154c
xyf-mac <xu_yanfeng@126.com>
2022-01-09 19:55:42 +0800

    注入代码，防止使用编辑器自带的package报错

---
+++ src/plugin/require-v3.ts
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import { ConcatSource } from 'webpack-sources'
+
+const code =
+    `(function (){
+    // 防止使用编辑器自带的package报错
+    const Path = require('path')
+    // @ts-ignore
+    module.paths.push(Path.join(Editor.App.path,'node_modules'));
+})();\n`
+export default class RequireV3 {
+    private options: any;
+
+    constructor(options: string) {
+        this.options = options;
+    }
+
+    apply(compiler: webpack.Compiler) {
+        compiler.hooks.emit.tap('require-v3', (compilation: webpack.Compilation) => {
+            const { options } = this;
+            const { assets } = compilation;
+
+            Object.keys(assets).forEach(e => {
+                if (/\.js$/.test(e)) {
+                    // @ts-ignore
+                    assets[e] = new ConcatSource(code, assets[e]);
+                }
+            })
+        })
+    }
+}
+
+
