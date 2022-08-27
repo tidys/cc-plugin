628580f529af64e5ab67d6f98b90d93d4ef19cf1
tidy-hn <tidysboy@gmail.com>
2022-08-27 12:36:21 +0800

    更新测试例

--- src/ui/app.vue
+++ src/ui/app.vue
@@ -1,56 +1,60 @@
 <template>
   <div style="display: flex;flex-direction: column;">
-    <cc-section name="TEST">
+    <CCSection name="TEST">
       <template v-slot:header>
         <div style="display: flex;flex:1;flex-direction: row;justify-content: flex-end;">
-          <cc-button>a</cc-button>
-          <cc-button>b</cc-button>
+          <CCButton>a</CCButton>
+          <CCButton>b</CCButton>
         </div>
       </template>
       <div>
-        <cc-prop name="test1">
-          <cc-input @change="onChangeText" v-model:value="value"></cc-input>
-        </cc-prop>
-        <cc-prop name="disabled">
-          <cc-input :disabled="true" value="disabled"></cc-input>
-        </cc-prop>
-        <cc-prop name="readonly">
-          <cc-input :readonly="true" value="readonly"></cc-input>
-        </cc-prop>
-        <cc-prop name="readonly && disabled">
-          <cc-input :disabled="true" :readonly="true" value="readonly"></cc-input>
-        </cc-prop>
-        <cc-prop name="test2">
-          <cc-select
+        <CCProp name="input">
+          <CCInput @change="onChangeText" v-model:value="value"></CCInput>
+        </CCProp>
+        <CCProp name="disabled">
+          <CCInput :disabled="true" value="disabled"></CCInput>
+        </CCProp>
+        <CCProp name="readonly">
+          <CCInput :readonly="true" value="readonly"></CCInput>
+        </CCProp>
+        <CCProp name="readonly && disabled">
+          <CCInput :disabled="true" :readonly="true" value="readonly"></CCInput>
+        </CCProp>
+        <CCProp name="test2">
+          <CCSelect
               @change="onChangeSelect"
-              v-model:data="selectData"
+              :data="selectData"
               v-model:value="selectValue">
-          </cc-select>
-        </cc-prop>
+          </CCSelect>
+        </CCProp>
       </div>
-    </cc-section>
-    <i class="iconfont icon-refresh"></i>
-    <div style="width: 100px; height: 100px;background: green;" class="iconfont icon-doc"></div>
-    <CCInputNumber :value=10 :min=1 style="width: 100px;"></CCInputNumber>
-    <CCColor></CCColor>
+    </CCSection>
+    <CCProp name="iconfont">
+      <i class="iconfont icon-refresh"></i>
+      <div class="iconfont icon-doc"></div>
+    </CCProp>
+    <CCProp name="color">
+      <CCColor></CCColor>
+    </CCProp>
+    <CCProp name="input number">
+      <CCInputNumber :value="1" :min="0"></CCInputNumber>
+    </CCProp>
   </div>
 </template>
 <script lang="ts">
-import {defineComponent, ref, onMounted} from 'vue'
-// import CcButton from './packages/cc-button/button.vue';
-// import CcSection from './packages/cc-section/index.vue';
-// import CcInput from './packages/cc-input/index.vue';
-// import CcProp from './packages/cc-prop/index.vue';
-// import CcSelect from './packages/cc-select/index.vue';
+import { defineComponent, ref, onMounted } from 'vue'
+import CCButton from './packages/cc-button/button.vue';
+import CCSection from './packages/cc-section/section.vue';
+import CCInput from './packages/cc-input/input.vue';
+import CCProp from './packages/cc-prop/prop.vue';
+import CCSelect from './packages/cc-select/select.vue';
 import CCInputNumber from './packages/cc-input-number/index.vue'
 import CCColor from "./packages/cc-color/color.vue";
 
 export default defineComponent({
   name: 'app',
   components: {
-    CCColor,
-    // CcSelect, CcProp, CcInput, CcSection, CcButton,
-    CCInputNumber,
+    CCColor, CCSelect, CCProp, CCInput, CCSection, CCButton, CCInputNumber,
   },
   setup() {
     const value = ref('123')
