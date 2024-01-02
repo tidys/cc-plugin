7a91641c78540a37fbcf795ccec0f00fe4b6ea61
xu_yanfeng <xu_yanfeng@126.com>
2024-01-02 19:44:38 +0800

    增加blob下载接口

--- src/ccp/util/download.ts
+++ src/ccp/util/download.ts
@@ -11,6 +11,9 @@ export class Download {
         } else {
             blob = new Blob([data]);
         }
+        this.downloadBlobFile(fileName, blob);
+    }
+    public static downloadBlobFile(fileName: string, blob: Blob) {
         let aLink = document.createElement('a');
         let evt = document.createEvent('HTMLEvents');
         evt.initEvent('click', true, true); //initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
