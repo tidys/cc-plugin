1c2cb4b1580380587c68f525ce1161a40017d492
xu_yanfeng <xu_yanfeng@126.com>
2024-01-03 16:44:37 +0800

    删除crlf的构建结果

--- bin/const.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreatorV3Limit = void 0;
-exports.CreatorV3Limit = {
-    minWidth: 800,
-    minHeight: 580,
-};
-//# sourceMappingURL=const.js.map
--- bin/plugin-api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PluginApi = void 0;
-class PluginApi {
-}
-exports.PluginApi = PluginApi;
-//# sourceMappingURL=plugin-api.js.map
